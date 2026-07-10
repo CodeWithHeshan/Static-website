5d2c5f68b9efa51202173163194218ca85389e55
CodeWithHeshan <sashinthaheshan3@gmail.com>
2026-07-10 09:19:15 +0530

    Add website file

---
+++ javascript/script.js
@@ -0,0 +1,19 @@
+function welcomeMessage() {
+    alert("Welcome to Spice Haven Restaurant!");
+}
+
+function aboutMessage() {
+    alert("Thank you for learning about our restaurant!");
+}
+
+function menuMessage() {
+    alert("Enjoy our delicious menu!");
+}
+
+function galleryMessage() {
+    alert("Welcome to our gallery!");
+}
+
+function contactMessage() {
+    alert("Thank you for contacting us!");
+}
